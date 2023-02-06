93a8966f69a920855f8811c2294f4274e58d9cb0
Adi-Noob <118145011+Adi-Noob@users.noreply.github.com>
2023-02-07 01:28:07 +0700

    Add files via upload

---
+++ config.jsx
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBqjCUCgk0_JGeWm3__CgRE96dYw8j_ths",
+  authDomain: "travel-app-04555.firebaseapp.com",
+  projectId: "travel-app-04555",
+  storageBucket: "travel-app-04555.appspot.com",
+  messagingSenderId: "451067904770",
+  appId: "1:451067904770:web:7dc15339ffd28c05dbf81b",
+  measurementId: "G-E2X06DVDJS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
